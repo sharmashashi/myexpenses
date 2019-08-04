83a4ba312a6c7658c2072156a7dad89b4c529542
shashi <sharmashashi940@gmail.com>
2019-08-04 17:25:09 +0545

    set up remaining salary day complete

--- lib/src/local_storage/sharedPreferences.dart
+++ lib/src/local_storage/sharedPreferences.dart
@@ -1,7 +1,9 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-void updateSharedPreferences({double totalIncome, double totalExpense}) async {
+void updateSharedPreferences(
+    {double totalIncome, double totalExpense, String salaryDayDate}) async {
   SharedPreferences myprefs = await SharedPreferences.getInstance();
   totalIncome != null ? myprefs.setDouble('totalIncome', totalIncome) : null;
   totalExpense != null ? myprefs.setDouble('totalExpense', totalExpense) : null;
+  salaryDayDate != null ? myprefs.setString('salaryDayDate', salaryDayDate) : null;
 }
